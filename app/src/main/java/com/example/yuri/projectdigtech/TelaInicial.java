9448723996315fbe0db9c650eeaed73cf61f0be4
YuriFernandes150 <yuri.fernandes150@gmail.com>
2019-02-11 09:45:35 -0200

    Adicionada tela de login

--- app/src/main/java/com/example/yuri/projectdigtech/TelaInicial.java
+++ app/src/main/java/com/example/yuri/projectdigtech/TelaInicial.java
@@ -1,5 +1,6 @@
 package com.example.yuri.projectdigtech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,17 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+
+        //aqui dentro as coisas acontecem antes da tela aparecer
+        try {
+            Thread.sleep(5000);
+            Intent myIntent = new Intent(TelaInicial.this, TelaLogin.class);
+            myIntent.putExtra("login", "telalogin");
+            TelaInicial.this.startActivity(myIntent);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
