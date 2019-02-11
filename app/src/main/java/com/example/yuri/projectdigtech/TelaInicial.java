298c648818d1314a08f8b4dfaf668d5bd7d1490b
YuriFernandes150 <yuri.fernandes150@gmail.com>
2019-02-11 09:58:20 -0200

    Adicionados Comentários

--- app/src/main/java/com/example/yuri/projectdigtech/TelaInicial.java
+++ app/src/main/java/com/example/yuri/projectdigtech/TelaInicial.java
@@ -15,8 +15,13 @@ public class TelaInicial extends AppCompatActivity {
         //aqui dentro as coisas acontecem antes da tela aparecer
         try {
             Thread.sleep(5000);
+            //Aqui a gente declara a itenção de interação entre duas classes! sempre use os parâmetros (classeatual.this, outraclasse.class)
             Intent myIntent = new Intent(TelaInicial.this, TelaLogin.class);
-            myIntent.putExtra("login", "telalogin");
+
+            //O putExtra serve para passar valores de uma tela para outra, no geral, ele é opcional
+            myIntent.putExtra("nomeuser", "Yuri Fernandes Gomes"); //Lembre se do tipo do segundo parâmetro, nesse caso, String
+
+            //Esse comando inicia a atividade, usando nossa intenção como parâmetro e passando quaisquer varáveis para a outra classa
             TelaInicial.this.startActivity(myIntent);
         } catch (InterruptedException e) {
             e.printStackTrace();
--- app/src/main/java/com/example/yuri/projectdigtech/TelaLogin.java
+++ app/src/main/java/com/example/yuri/projectdigtech/TelaLogin.java
@@ -1,5 +1,6 @@
 package com.example.yuri.projectdigtech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class TelaLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        //Para usar os Extras que colocamos na classe anterior, devemos declarar outra intenção, mas pegando a que acabamos de usar pra chegar nesssa tela
+        Intent intent = getIntent();//get intent pega a última instância conhecida de intenção
+        String nomeUsuario = intent.getStringExtra("nomeuser");//aqui vc declara uma variável do msm tipo do segundo parâmetro do seu putExtra na classe anterior!
+        //Agora vc pode usar o nomeUsuário que vc declarou na Activity anterior! Lembre-se de usar o tipo certo!
     }
 }
