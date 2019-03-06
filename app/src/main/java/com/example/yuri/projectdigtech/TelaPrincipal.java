deff658f0675e0867d680ddb0fc85f0b8855d8b1
f4r0fA <luizinhos04@hotmail.com>
2019-03-06 17:48:03 -0300

    Update Comit

--- app/src/main/java/com/example/yuri/projectdigtech/TelaPrincipal.java
+++ app/src/main/java/com/example/yuri/projectdigtech/TelaPrincipal.java
@@ -2,8 +2,15 @@ package com.example.yuri.projectdigtech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button btnLoggin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
